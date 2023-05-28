a2720f3f33aba420e798cf3438acfc97c5d54824
Troels Korreman <troels.korreman@gmail.com>
2023-05-28 14:50:35 +0200

    Expand command options

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,46 @@
 use std::os::unix::process::CommandExt;
+use std::path::PathBuf;
 use std::process::{Command, ExitCode};
 
-use clap::{Parser, Subcommand};
+use clap::{Args, ColorChoice, Parser, Subcommand};
 
 #[derive(Debug, Clone, Parser)]
 #[command(author, version, about)]
 struct Cmd {
+    /// Print the corresponding command to stdout instead of running it.
+    #[arg(long)]
+    generate_command: bool,
+    #[command(flatten)]
+    opts: GlobalOpts,
+    #[command(subcommand)]
+    sub: SubCmd,
+}
+
+#[derive(Debug, Clone, Args)]
+struct GlobalOpts {
     /// Display debug messages.
     #[arg(short, long)]
     verbose: bool,
-
-    /// Print the generated pacman command to stdout instead of running it.
-    #[arg(long)]
-    generate_command: bool,
 
     /// Simulate a test run without performing any changes.
     #[arg(long)]
     simulate: bool,
 
-    #[command(subcommand)]
-    sub: SubCmd,
+    /// Specify when to colorize output.
+    #[arg(short, long, value_enum, default_value_t = ColorChoice::Auto)]
+    color: ColorChoice,
+
+    /// Specify an alternate configuration file.
+    #[arg(long, value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    /// Specify an alternate database location.
+    #[arg(long, value_name = "DIR")]
+    dbpath: Option<PathBuf>,
+
+    /// Specify an alternate directory for GnuPG.
+    #[arg(long, value_name = "DIR")]
+    gpgdir: Option<PathBuf>,
 }
 
 #[derive(Debug, Clone, Subcommand)]
@@ -27,12 +48,14 @@ enum SubCmd {
     /// Install or reinstall packages.
     Install {
         /// Packages to install/reinstall.
+        #[arg(value_name = "PACKAGE")]
         packages: Vec<String>,
     },
 
     /// Remove packages.
     Remove {
         /// Packages to remove.
+        #[arg(value_name = "PACKAGE")]
         packages: Vec<String>,
         /// Do not remove config files.
         #[arg(long)]
@@ -51,24 +74,28 @@ enum SubCmd {
     /// Search for a package.
     Search {
         /// Query strings to search for, regexes used for matching.
+        #[arg(value_name = "QUERY")]
         queries: Vec<String>,
     },
 
     /// Print info on packages.
     Info {
         /// Packages to display info on.
+        #[arg(value_name = "PACKAGE")]
         packages: Vec<String>,
     },
 
     /// Mark packages as directly installed. (TODO: better description)
     Pin {
         /// Packages to pin.
+        #[arg(value_name = "PACKAGE")]
         packages: Vec<String>,
     },
 
     /// Unmark packages as directly installed. (TODO: better description)
     Unpin {
         /// Packages to unpin.
+        #[arg(value_name = "PACKAGE")]
         packages: Vec<String>,
     },
 
@@ -79,23 +106,29 @@ enum SubCmd {
         /// Use ASCII characters for tree formatting.
         #[arg(short, long)]
         ascii: bool,
-        /// Colorize output.
-        #[arg(short, long)]
-        color: bool,
         /// Limit the depth of recursion.
         #[arg(short, long, value_name = "NUMBER")]
         depth: Option<u32>,
-        // TODO: Rest of arguments.
+        /// Limit depth of optional.
+        #[arg(long, value_name = "NUMBER")]
+        depth_optional: Option<u32>,
+        /// List package dependants instead of dependencies.
+        #[arg(short, long)]
+        reverse: bool,
     },
 }
 
 impl SubCmd {
-    fn generate_command(self, simulate: bool) -> Vec<String> {
-        let mut cmd = if !simulate {
-            vec!["pacman".to_owned()]
-        } else {
-            vec!["pacman".to_owned(), "--print".to_owned()]
+    fn generate_command(self, global: &GlobalOpts) -> Vec<String> {
+        let mut cmd = vec!["pacman".to_owned()];
+        if global.simulate {
+            cmd.push("--print".to_owned());
         };
+        if global.verbose {
+            cmd.push("--debug".to_owned());
+        }
+        cmd.push("--color".to_owned());
+        cmd.push(global.color.to_string());
         match self {
             SubCmd::Install { packages } => {
                 cmd.push("-S".to_owned());
@@ -107,7 +140,7 @@ impl SubCmd {
                 keep_orphans,
                 uproot,
             } => {
-                let mut cmd_arg = String::from("R");
+                let mut cmd_arg = String::from("-R");
                 if !keep_orphans {
                     cmd_arg.push('s');
                 }
@@ -145,20 +178,36 @@ impl SubCmd {
             SubCmd::Tree {
                 package,
                 ascii,
-                color,
                 depth,
+                depth_optional,
+                reverse,
             } => {
                 cmd = vec!["pactree".to_owned()];
                 let mut cmd_arg = String::from("-");
                 if ascii {
                     cmd_arg.push('a');
                 }
+                let color = global.color == ColorChoice::Always
+                    || global.color == ColorChoice::Auto && atty::is(atty::Stream::Stdout);
                 if color {
                     cmd_arg.push('c');
                 }
-                if let Some(depth) = depth {
-                    cmd_arg.push('d');
-                    cmd.push(format!("{depth}"));
+                // TODO: More global configuration options.
+                if reverse {
+                    cmd_arg.push('r');
+                }
+                if let Some(d) = depth {
+                    cmd.push("-d".to_owned());
+                    cmd.push(format!("{d}"));
+                }
+                if let Some(dopt) = depth_optional {
+                    cmd.push(format!("--optional={dopt}"));
+                }
+                if global.verbose {
+                    cmd.push("--debug".to_owned());
+                }
+                if cmd_arg != "-" {
+                    cmd.push(cmd_arg);
                 }
                 cmd.push(package);
                 cmd
@@ -169,7 +218,7 @@ impl SubCmd {
 
 fn main() -> ExitCode {
     let args = Cmd::parse();
-    let mut command = args.sub.generate_command(args.simulate);
+    let mut command = args.sub.generate_command(&args.opts);
     if args.generate_command {
         println!("{}", command.join(" "));
         ExitCode::SUCCESS
